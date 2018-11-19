3d85c4d3761a4e2700c3c3e11d741aa8cc1f0baa
M7MDSAM0567379889 <45179393+M7MDSAM0567379889@users.noreply.github.com>
2018-11-19 20:19:27 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const prefix = "!"
+const id = ["317969406300782592","316143612620570634"]
+const client = new Discord.Client();
+var token = process.env.TOKEN
+
+client.on('ready', () => {
+  console.log(`Hi ${client.user.tag} , This Code by : Ameer `);
+  console.log('')
+  console.log(`i Have  [ " ${client.guilds.size} " ]`);
+});
+
+const developers = id
+const adminprefix = prefix;
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send("**:white_check_mark: | The Watching Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send("**:white_check_mark: | The Listening Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/i_kahrba999");
+      message.channel.send("**:white_check_mark: | The Streaming Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+
+
+
+client.login(token);
